fbc281242c59460994d4b6f16320558ebd78748d
Надія Ткач <nadiya77723@gmail.com>
2023-01-29 10:43:22 +0200

    /

--- controllers/contacts/add.js
+++ controllers/contacts/add.js
@@ -2,7 +2,6 @@ const contactsOperations = require("../../models/contacts");
 
 const add = async (req, res) => {
   const result = await contactsOperations.addContact(req.body);
-  console.log(result);
   res.status(201).json({
     status: "success",
     code: 201,
