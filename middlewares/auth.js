ab0850dc1bda86ca1568f168e49be4fac6ea3cf2
Надія Ткач <nadiya77723@gmail.com>
2023-02-09 20:28:45 +0200

    Зробила міделвару для auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,40 +1,34 @@
-
-const { Unauthorized } = require("http-errors");
+const {Unauthorized} = require("http-errors");
 const jwt = require("jsonwebtoken");
 
 const { User } = require("../models");
 
-const {SECRET_KEY} = process.env;
+const { SECRET_KEY } = process.env;
 
 const auth = async (req, res, next) => {
-  const { authorization = "" } = req.headers;
-  const { bearer, token } = authorization.split(" ");
+  const {authorization = "" } = req.headers;
+  const [bearer, token] = authorization.split(" ");
 
   try {
     if (bearer !== "Bearer") {
-      throw new Unauthorized("Not authorized");
+      throw new Unauthorized("Not authorized")
     }
-
-     if (!token) {
-    throw Unauthorized("Not authorized");
-  }
-
+  
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
-   
 
+    req.user = user;
+   
     if (!user) {
       throw new Unauthorized("Not authorized");
     }
-
-    req.user = user;
-    next();
   } catch (error) {
-    if(error.message === "Invalid sugnature") {
-      error.status = 401;
+    if (error.name === "Invalid sugnature") {
+      throw Unauthorized("jwt token is not valid");
     }
-    next(error);
+    throw error;
   }
+  next();
 };
 
 module.exports = auth;
