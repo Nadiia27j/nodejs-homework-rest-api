62bc681a22ad4c18e1d63d92034bb01db688c8d8
Надія Ткач <nadiya77723@gmail.com>
2023-02-13 18:29:14 +0200

    закінчує роботу з аватаркою

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,6 +1,7 @@
 const {Conflict} = require("http-errors");
 const {User} = require("../../models/user");
 const bcrypt = require("bcryptjs");
+const gravatar = require("gravatar");
 
 
 const register = async(req, res) => {
@@ -11,9 +12,11 @@ const register = async(req, res) => {
       throw new Conflict(`User with ${email} already exist`)
    }
 
+   const avatarURL = gravatar.url(email);
+
    const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
 
-   const result = await User.create({email, password: hashPassword});
+   const result = await User.create({email, password: hashPassword, avatarURL});
    res.status(201).json({
     status: "success",
     code: 201,
